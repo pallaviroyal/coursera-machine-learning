7cf3956fd1aeb39740da6e48e3883d59ff3bffb2
Pallavi Ramicetty <pallavi.ramicetty@gmail.com>
2017-05-22 12:33:07 +0530

    ex1 : Computed cost function

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,7 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X * theta; % predictions on all examples
 
+squeredError = (predictions-y) .^2; % squared errors
+
+J = 1/(2*m) * sum(squeredError); % cost function
 
 
 
