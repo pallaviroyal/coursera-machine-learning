085295832338f7cb1efaea6dbf2cf1ed5c1ca70c
pallavi royal <pallavi.ramicetty@gmail.com>
2017-05-24 15:04:11 +0530

    ex2 : cost function with regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+h = sigmoid(X * theta);
+thetaCopy = theta;
+thetaCopy([1],:) = [];
+J = (1/m) * ( ((-y)' * log(h) - (1 - y)' * log(1-h)) + (lambda/2) * sum(thetaCopy.^2))  ;
+x_transpose = X';
+grad(1,:) = (1/m) * (X'(1,:) * (h - y));
+grad([2:end],:) = (1/m) * ( (X'([2:end],:) * (h - y)) + ( lambda * thetaCopy));
 
 % =============================================================
 
